53057f8f3bc1b66ff7a7debb5179abb2bd509ef6
Juliette Schabanel <jschabanel@clipper.ens.fr>
2026-01-30 15:35:00 +0100

    init

--- topsurf/__init__.py
+++ topsurf/__init__.py
@@ -1,7 +1,9 @@
 r"""
-Cell decompositions of surfaces in Python
+Cell decompositions of surfaces and combinatorial maps in Python.
 """
 
 from topsurf.version import version as __version__
 
 from topsurf.oriented_map import OrientedMap
+
+from topsurf.colored_map import ColoredOrientedMap
